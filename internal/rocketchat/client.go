d0a7abf492317173696f4e95ee101b93cb8a547a
leberKleber <max.marche@live.de>
2019-04-03 19:29:11 +0200

    fix travis-ci integration

--- internal/rocketchat/client.go
+++ internal/rocketchat/client.go
@@ -49,7 +49,7 @@ func (rcc *Client) handleIncoming(msgType int, msgPayload []byte) {
 			log.WithError(err).Error("Failed to unmarshal msg payload")
 		}
 
-		log.Infof("Incoming text-message: %d  %v", msg, string(msgPayload))
+		log.Infof("Incoming text-message: %v  %v", msg, string(msgPayload))
 
 		switch msg.Message {
 		case "ping":
